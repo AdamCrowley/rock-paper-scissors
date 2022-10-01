b5257c2e0bb780ea3fb0cb00e8e06d993df4a6fc
Adam Crowley <adamcrowley@gmail.com>
2022-10-01 14:34:52 +1300

    Refactor code

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
 function getComputerChoice() {
-
-	//create array of 3 choices
 	let moves = ['rock', 'paper', 'scissors']
-	let move = '';
+	let randomNumber = Math.floor(Math.random()*3); //generate random number between 0 and 2
+	let move = moves[randomNumber];
 
-	//generate random number between 0 and 2
-	let randomNumber = Math.floor(Math.random()*3);
-
-	//use random number to select move from array
-	move = moves[randomNumber];
-
-	//return move as string
 	return move;
-
 }
 
 function playRound(roundNumber, playerSelection, computerSelection) {
-
-	console.log(`Player Selection: ${playerSelection}. Computer Selection: ${computerSelection}.`);
-
-	//convert playerSelection string to all lowercase
-	playerSelection = playerSelection.toLowerCase();
-	//create round result variable
 	let roundResult = '';
+
+	playerSelection = playerSelection.toLowerCase();
 
 	//resolve round winner
 	if (playerSelection === 'rock') {
@@ -63,35 +50,35 @@ function playRound(roundNumber, playerSelection, computerSelection) {
 		}
 	}
 
-	//log to console the round result
-	console.log(`Round number: ${roundNumber}. Round result: ${roundResult}. Player Score: ${playerScore}. Computer Score: ${computerScore}`);
+	console.log(`
+Player Selection: ${playerSelection}.
+Computer Selection: ${computerSelection}.	
+Round number: ${roundNumber}.
+Round result: ${roundResult}.
+Player Score: ${playerScore}. Computer Score: ${computerScore}
+	`);
 	
 	return;
-
 }
 
 function game() {
 	let playerSelection = '';
 	let computerSelection = '';
 	let gameOutcome = '';
+
 	//play 5 rounds of game
 	for (let i = 0; i < 5; i++) {
-		//create roundNumber variable
 		let roundNumber = i + 1;
-		//prompt player to receive input
 		playerSelection = prompt(`Please enter 'Rock', 'Paper' or 'Scissors'`);
-		//get computer selection
 		computerSelection = getComputerChoice();
-		//play a round
 		playRound(roundNumber, playerSelection, computerSelection);
 	}
 
-	// reports winner or loser 
+	// report winner or loser
 	if (playerScore > computerScore) { gameOutcome = 'You Win!' }
 	else if (playerScore < computerScore ) { gameOutcome = 'You Lose!' }
 	else { gameOutcome = `It's a Draw!`}
 
-	//log game outcome to console
 	console.log(`Game Outcome: ${gameOutcome} Total Player Score: ${playerScore}. Total Computer Score: ${computerScore}`);
 }
 
